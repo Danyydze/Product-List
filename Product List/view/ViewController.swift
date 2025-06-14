62dc80be9cc5410a5a5ce607b57274b57195c460
danyydze <damarkov@edu.hse.ru>
2025-06-14 15:41:34 +0300

    Удаление Сториборда и настройка папок

---
+++ Product List/view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Product List
+//
+//  Created by Данил Марков on 14.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
