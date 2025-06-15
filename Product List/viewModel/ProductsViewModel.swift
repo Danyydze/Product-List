130c0f6c51a4177b7cfd6247acf6df8eec5d37ca
Danyydze <danilamarkov2803@icloud.com>
2025-06-15 12:38:58 +0300

    Добавление вью модели

--- Product List/viewModel/ProductsViewModel.swift
+++ Product List/viewModel/ProductsViewModel.swift
@@ -5,3 +5,21 @@
 //  Created by Данил Марков on 14.06.2025.
 //
 
+import UIKit
+
+class ProductsViewModel {
+    private(set) var products: [Product] = []
+    
+    func loadProducts(complection: @escaping () -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            if let url = Bundle.main.url(forResource: "products", withExtension: "json"),
+               let data = try? Data(contentsOf: url),
+               let productsData = try? JSONDecoder().decode([Product].self, from: data) {
+                self.products = products
+            }
+            DispatchQueue.main.async {
+                complection()
+            }
+        }
+    }
+}
