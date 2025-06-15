e97d2e0700bc248c652aa3fb581adb4c9a6f2044
Danyydze <danilamarkov2803@icloud.com>
2025-06-15 20:04:39 +0300

    Добавлена модель вьюхи

--- Product List/viewModel/ProductsViewModel.swift
+++ Product List/viewModel/ProductsViewModel.swift
@@ -8,18 +8,35 @@
 import UIKit
 
 class ProductsViewModel {
-    private(set) var products: [Product] = []
+    private(set) var allProducts: [Product] = []
+    private(set) var filteredProducts: [Product] = []
     
-    func loadProducts(complection: @escaping () -> Void) {
+    func loadProducts(completion: @escaping () -> Void) {
         DispatchQueue.global(qos: .background).async {
-            if let url = Bundle.main.url(forResource: "products", withExtension: "json"),
-               let data = try? Data(contentsOf: url),
-               let productsData = try? JSONDecoder().decode([Product].self, from: data) {
-                self.products = products
+            do {
+                if let url = Bundle.main.url(forResource: "products", withExtension: "json"),
+                   let data = try? Data(contentsOf: url),
+                   let productsData = try? JSONDecoder().decode([Product].self, from: data) {
+                    self.allProducts = productsData
+                    self.filteredProducts = productsData
+                }
+            } catch {
+                print("Error loading products: \(error)")
             }
             DispatchQueue.main.async {
-                complection()
+                completion()
             }
+        }
+    }
+    
+    func filterProducts(by segmentIndex: Int) {
+        switch segmentIndex {
+        case 0:
+            filteredProducts = allProducts
+        case 1:
+            filteredProducts = allProducts.filter { $0.discount == 0 }
+        default:
+            filteredProducts = allProducts
         }
     }
 }
